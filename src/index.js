68cc7237fe9d237beda81aaa8012ee44f6907e15
Wellington Soares <wsoares@uolinc.com>
2019-04-05 19:51:15 -0300

    setup route

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Notfound from './Notfound';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="/" exact={true}  component={App} />
-            <Route path="*" component={Notfound} />
+            <Route path="*" component={App} />
         </Switch>
     </ BrowserRouter>
     , document.getElementById('root'));
