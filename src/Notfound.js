ce11fdc4d0e5774a729d119944ba44bc54389c84
Wellington Soares <wsoares@uolinc.com>
2019-04-05 19:48:21 -0300

    setup route

--- src/Notfound.js
+++ src/Notfound.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Notfound extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="Notfound">
+        <header className="Notfound-header">
+          <img src={logo} className="Notfound-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. olha que legal
+            asdadadasdasdasdassada
           </p>
           <a
-            className="App-link"
+            className="Notfound-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -25,4 +25,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Notfound;
