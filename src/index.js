5e6067ca3ab57aa337a7401e4e365ce4f12355ab
Wellington Soares <wsoares@uolinc.com>
2019-04-05 19:54:01 -0300

    setup route

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="*" component={App} />
+            <Route path="/" component={App} />
         </Switch>
     </ BrowserRouter>
     , document.getElementById('root'));
