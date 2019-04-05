ae539edc5dff32a2d93c51055c56cc8b454c233b
Wellington Soares <wsoares@uolinc.com>
2019-04-05 19:16:37 -0300

    Updates

---
+++ precache-manifest.949c804adf67544454c401a0a2f8d6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b3468ecff83be784c4",
+    "url": "/luizalabs-app/static/css/main.7c6458d5.chunk.css"
+  },
+  {
+    "revision": "39b3468ecff83be784c4",
+    "url": "/luizalabs-app/static/js/main.41a4f359.chunk.js"
+  },
+  {
+    "revision": "b621a9723c97c78bd3b6",
+    "url": "/luizalabs-app/static/js/runtime~main.3b8a8105.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/luizalabs-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/luizalabs-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f3a508d18568c079f077b291eac4fc12",
+    "url": "/luizalabs-app/index.html"
+  }
+];
